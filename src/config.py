febded78e9bf41b98f9da4dde76104bfbddb1692
lintosunny <lintosunny111@gmail.com>
2025-02-28 21:59:32 +0530

    updated the structure

---
+++ src/config.py
@@ -0,0 +1,8 @@
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ACCESS_TOKEN = os.getenv('FACEBOOK_ACCESS_TOKEN')
+PAGE_ID = os.getenv('PAGE_ID')
+GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
