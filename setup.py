d3fbdcff53e9d7504c17c8338f57a4a744912835
lintosunny <lintosunny111@gmail.com>
2025-03-02 12:28:48 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str) -> List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
 
 setup(
-    name="facebook-comment-bot",
-    version="0.1",
-    packages=find_packages(),
-    install_requires=[
-        "requests",
-        "google-generativeai",
-        "python-dotenv"
-    ]
+    name='facebook-comment-bot',
+    version='0.0.1',
+    author='Linto Sunny',
+    author_email='lintosunny111@gmail.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()  # This will get the local packages
 )
